a2b2fb6f7c851d916030dd91d333a7670e174254
John Yenter-Briars <jyenterbriars@gmail.com>
2022-03-09 07:51:03 -0800

    add sqllite pagkage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
+    let connection = sqlite::open(":memory:").unwrap();
+
+    connection
+        .execute(
+            "
+        CREATE TABLE users (name TEXT, age INTEGER);
+        INSERT INTO users VALUES ('Alice', 42);
+        INSERT INTO users VALUES ('Bob', 69);
+        ",
+        )
+        .unwrap();
     println!("Hello, world!");
 }
