d9570e8836e20e7b5aa1c70d9cc026b6fe511d1d
John Yenter-Briars <jyenterbriars@gmail.com>
2022-03-17 10:29:10 -0500

    add to_serde method on reponses

--- src/models/responses/neweventresponse.rs
+++ src/models/responses/neweventresponse.rs
@@ -1,17 +1,29 @@
 use serde::Serialize;
 
 #[derive(Serialize)]
-pub struct NewEventResponse { 
+pub struct NewEventResponse {
     status_code: u32,
     message: String,
 }
 
 impl NewEventResponse {
     pub fn created() -> Self {
-        NewEventResponse{status_code: 201, message: "Event created".to_string()}
+        NewEventResponse {
+            status_code: 201,
+            message: "Event created".to_string(),
+        }
     }
 
     pub fn error(message: String) -> Self {
-        NewEventResponse{status_code: 500, message}
+        NewEventResponse {
+            status_code: 500,
+            message,
+        }
+    }
+
+    pub fn as_serde_string(self) -> String {
+        let response_string =
+            serde_json::to_string(&self).expect("Unable to parse response object!");
+        response_string
     }
 }
