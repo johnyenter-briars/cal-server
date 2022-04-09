5cd717736d4c8190e33632084b3beda698e2f625
John Yenter-Briars <jyenterbriars@gmail.com>
2022-04-09 17:35:13 -0500

    refac api key into modules

---
+++ src/server/httpserver.rs
@@ -0,0 +1,34 @@
+use crate::routes::{
+    caluserroutes::{create_caluser, get_caluser},
+    eventroutes::{create_event, get_events},
+    seriesroutes::{create_series, get_series},
+};
+use actix_web::{
+    middleware::Logger,
+    App, HttpServer,
+};
+
+use super::apikey::ApiKey;
+
+pub async fn build_and_run_server(
+    domain: String,
+    port: u16,
+    key_value: String,
+) -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    HttpServer::new(move || {
+        App::new()
+            .service(create_event)
+            .service(get_events)
+            .service(create_caluser)
+            .service(get_caluser)
+            .service(create_series)
+            .service(get_series)
+            .wrap(Logger::new("%a %{User-Agent}i %r %s %U %{Content-Type}i"))
+            .wrap(ApiKey::new(key_value.clone()))
+    })
+    .bind((domain, port))?
+    .run()
+    .await
+}
