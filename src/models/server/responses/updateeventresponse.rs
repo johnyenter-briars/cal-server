59385e044825a532e51b2fb1059d42e19ec26de6
John Yenter-Briars <jyenterbriars@gmail.com>
2022-05-25 07:53:58 -0500

    clippy

--- src/models/server/responses/updateeventresponse.rs
+++ src/models/server/responses/updateeventresponse.rs
@@ -2,7 +2,7 @@ use actix_web::{http::header::ContentType, HttpResponse};
 use serde::Serialize;
 use uuid::Uuid;
 
-use crate::models::traits::validate::Validatable;
+
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
